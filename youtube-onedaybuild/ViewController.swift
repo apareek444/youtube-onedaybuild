909617b609dc6a850064e218e67254477c1e72b5
Aryan Pareek <apareek369@gmail.com>
2020-08-02 15:15:23 -0700

    Initial Xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Aryan Pareek on 8/2/20.
+//  Copyright © 2020 Aryan Pareek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
